967271995f2a7932413b1ae300fd545509b28195
Suresh Kandel <asuka003@edu.xamk.fi>
2019-09-11 11:49:39 +0300

    Add project files.

---
+++ Assign2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
